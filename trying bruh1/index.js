e04b61d8780bd1e4534b70d842ca3068c1b7b5cb
Black Soul <ghalyomar492@gmail.com>
2024-09-20 22:00:27 +0300

    Your commit message

--- trying bruh1/index.js
+++ trying bruh1/index.js
@@ -1,2 +1,30 @@
 'use strict'
-console.log(Math.random());
+
+let markMass1 = 78;  
+let markHeight1 = 1.69;  
+let johnMass1 = 92;  
+let johnHeight1 = 1.95;  
+
+let markMass2 = 95;  
+let markHeight2 = 1.88;  
+let johnMass2 = 85;  
+let johnHeight2 = 1.76; 
+
+let markBMI1 = markMass1 / (markHeight1 ** 2);
+let johnBMI1 = johnMass1 / (johnHeight1 ** 2);
+
+let markBMI2 = markMass2 / (markHeight2 ** 2);
+let johnBMI2 = johnMass2 / (johnHeight2 ** 2);
+
+let markHigherBMI1 = markBMI1 > johnBMI1;
+let markHigherBMI2 = markBMI2 > johnBMI2;
+
+console.log("Test Data 1:");
+console.log("Mark's BMI:", markBMI1.toFixed(2));
+console.log("John's BMI:", johnBMI1.toFixed(2));
+console.log("Does Mark have a higher BMI than John?", markHigherBMI1);
+
+console.log("\nTest Data 2:");
+console.log("Mark's BMI:", markBMI2.toFixed(2));
+console.log("John's BMI:", johnBMI2.toFixed(2));
+console.log("Does Mark have a higher BMI than John?", markHigherBMI2);
